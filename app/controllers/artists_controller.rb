d9af9e437592c10cba8bb86ce39f1ca7ef7fb4fb
bstizzle <whereyouben@gmail.com>
2020-11-23 12:24:12 -0500

    built artist/instrument rails app

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,28 @@
 class ArtistsController < ApplicationController
+    def index
+        @artists = Artist.all
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.create(artist_params)
+        if @artist.save
+            redirect_to artist_path(@artist)
+        else 
+            render 'new'
+        end 
+    end
+
+    private
+
+    def artist_params
+        params.require(:artist).permit(:name, :age, :title)
+    end
 end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,2 +1,6 @@
 class Instrument < ApplicationRecord
+    has_many :artist_instruments
+    has_many :artists, through: :artist_instruments
+
+    validates :name, uniqueness: true
 end
